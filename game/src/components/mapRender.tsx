0cf023feb7fae68fa48205e16dfa7faf1a451809
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-12 15:33:19 +0100

    Commend code was for @diamdesign to see that it works correct with animations

--- game/src/components/mapRender.tsx
+++ game/src/components/mapRender.tsx
@@ -1,6 +1,6 @@
 import '../css/MapRender.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 //Check if array is an array of arrays
 interface MapRenderProps {
@@ -10,18 +10,18 @@ interface MapRenderProps {
 // import map1 from './maps/map1.json';
 {/* <MapRender initialMapData={map1.mapdata} /> */ }
 
+
+
 export function MapRender({ initialMapData }: MapRenderProps) {
   const [mapData, setMapData] = useState(initialMapData);
-
-  if (!mapData) {
-    return <div>Loading...</div>;
-  }
+  // const [playerDirection, setPlayerDirection] = useState('down');
 
   //will be used to get the class name for each symbol in the map
   const getClassNameForSymbol = (symbol: string) => {
     switch (symbol) {
       case '-': return 'empty';
-      case 'P': return 'player';
+      // case 'P': return `player-${playerDirection} playerwalk${playerDirection}`;
+      case 'P': return `player`;
       case 'B': return 'box';
       case ',': return 'ground';
       case 'I': return 'boxindicator';
@@ -30,13 +30,44 @@ export function MapRender({ initialMapData }: MapRenderProps) {
     }
   };
 
+  
+  // useEffect(() => {
+  //   const handleKeyDown = (event: KeyboardEvent) => {
+  //     switch (event.key) {
+  //       case 'ArrowUp':
+  //         setPlayerDirection('up');
+  //         break;
+  //       case 'ArrowDown':
+  //         setPlayerDirection('down');
+  //         break;
+  //       case 'ArrowLeft':
+  //         setPlayerDirection('left');
+  //         break;
+  //       case 'ArrowRight':
+  //         setPlayerDirection('right');
+  //         break;
+  //     }
+  //   };
+
+  //   window.addEventListener('keydown', handleKeyDown);
+
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
+
   return (
     <div className="grid-container">
       {mapData.map((row, rowIndex) => (
         <div key={rowIndex} className="grid-row">
-          {row.map((column, columnIndex) => (
-            <div key={columnIndex} className={`grid-item ${getClassNameForSymbol(column)}`}></div>
-          ))}
+          {row.map((column, columnIndex) => {
+            const className = getClassNameForSymbol(column);
+            return (
+              <div key={columnIndex} className={`grid-item ${className}`}>
+                {className === 'boxindicator' && <div className="boxindicator-container"></div>}
+              </div>
+            );
+          })}
         </div>
       ))}
     </div>
