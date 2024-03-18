210e65bc954ac925cd4aebf837fddc631f50f60a
mansooriabbas <mansoori.abbas@outlook.com>
2024-03-18 12:50:25 +0100

    added level display to app so that selectlevel can receive the maps as props

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -10,73 +10,82 @@ import { FullscreenToggle } from "./components/fullscrenToggle";
 import musicSong from "./assets/neon-gaming-128925.mp3";
 import "./App.css";
 import "./css/MediaQueries.css";
-
-import { LevelDisplay } from "./components/LevelDisplay";
+import allMaps from "./maps/maps";
 
 type Page = "start" | "selectlevel" | "play" | "credits";
 
+
 function App() {
-	const [currentPage, setCurrentPage] = useState("start");
-	const [settings, toggleSettings] = useState(false);
+    const [currentPage, setCurrentPage] = useState<Page>("start");
+    const [settings, toggleSettings] = useState<boolean>(false);
+    const [level, setLevel] = useState<number >(1); // lvl state starts at 1
 
-	function handleToggleSettings() {
-		if (settings) {
-			toggleSettings(false);
-		} else {
-			toggleSettings(true);
-		}
-		playSound("click", 0.25);
-		playSound("swoosh", 0.25);
-	}
+    function handleToggleSettings() {
+        toggleSettings(!settings);
+        playSound("click", 0.25);
+        playSound("swoosh", 0.25);
+    }
 
-	function handleMouseOver() {
-		playSound("hover", 0.15);
-	}
+    function handleMouseOver() {
+        playSound("hover", 0.15);
+    }
 
-	const handlePageChange = (page: Page) => {
-		setCurrentPage(page);
-	};
+    const handlePageChange = (page: Page) => {
+        setCurrentPage(page);
+    };
 
-	return (
-		<>
-			<button
-				id="btn-settings"
-				className="button"
-				onClick={handleToggleSettings}
-				onMouseOver={handleMouseOver}
-			></button>
+    // Function to handle level change
+    const handleLevelChange = () => {
+        const mapKeys = Object.keys(allMaps);
+        const nextIndex = (level % mapKeys.length) + 1;
+        setLevel(nextIndex);
+    };
 
-			<FullscreenToggle />
-			{settings && <Settings />}
+    // Calculate map count
+    const mapCount = Object.keys(allMaps).length;
 
-			{/* Music Player */}
-			{<Music audio={musicSong} />}
+    return (
+        <>
+            <button
+                id="btn-settings"
+                className="button"
+                onClick={handleToggleSettings}
+                onMouseOver={handleMouseOver}
+            ></button>
 
-			<div id="startpageui">
-				{/* StartPage UI */}
-				{currentPage === "start" && <StartPageUI onPageChange={handlePageChange} />}
+            <FullscreenToggle />
+            {settings && <Settings />}
 
-				{/* Select Level */}
-				{currentPage === "selectlevel" && <SelectLevel onPageChange={handlePageChange} />}
+            {/* Music Player */}
+            {<Music audio={musicSong} />}
 
-				{/* Credits */}
-				{currentPage === "credits" && <Credits onPageChange={handlePageChange} />}
+            <div id="startpageui">
+                {/* StartPage UI */}
+                {currentPage === "start" && <StartPageUI onPageChange={handlePageChange} />}
 
-				{/* Play */}
-				{currentPage === "play" && <Play onPageChange={handlePageChange} />}
+                {/* Select Level*/} 
+                {currentPage === "selectlevel" && <SelectLevel onPageChange={handlePageChange} mapCount={mapCount} currentLevel={level} onLevelChange={handleLevelChange}  />}
+				
 
-				<div id="space">
-					<div className="stars"></div>
-					<div className="stars"></div>
-					<div className="stars"></div>
-					<div className="stars"></div>
-					<div className="stars"></div>
-				</div>
-				<div id="retrogrid"></div>
-				<div id="copyright">© 2024 Studio5</div>
-			</div>
-		</>
-	);
+
+                {/* Credits */}
+                {currentPage === "credits" && <Credits onPageChange={handlePageChange} />}
+
+                {/* Play */}
+                {currentPage === "play" && <Play onPageChange={handlePageChange} />}
+
+                <div id="space">
+                    <div className="stars"></div>
+                    <div className="stars"></div>
+                    <div className="stars"></div>
+                    <div className="stars"></div>
+                    <div className="stars"></div>
+                </div>
+                <div id="retrogrid"></div>
+                <div id="copyright">© 2024 Studio5</div>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- game/src/pages/SelectLevel.tsx
+++ game/src/pages/SelectLevel.tsx
@@ -1,116 +1,121 @@
 import { useState, useEffect } from "react";
 import { SelectPageProps } from "./../components/InterfacePages";
 import { playSound } from "./../components/playSound";
+import allMaps from "../maps/maps";
 
-export function SelectLevel({ onPageChange }: SelectPageProps) {
-	const [mapFiles, setMapFiles] = useState<string[]>([]);
-	const [currentPage, setCurrentPage] = useState<number>(0);
+interface SelectLevelProps extends SelectPageProps {
+    mapCount: number;
+	currentLevel?: number
+	onLevelChange: () => void;
+}
 
-	// Set amount of maps & maps per page
-	const mapCount = 60;
-	const perPage = 20;
-	const startIndex = currentPage * perPage;
-	const endIndex = Math.min((currentPage + 1) * perPage, mapFiles.length);
+export function SelectLevel({ onPageChange, mapCount }: SelectLevelProps) {
+    const [mapFiles, setMapFiles] = useState<string[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(0);
 
-	useEffect(() => {
-		// Set mapFiles with some map data
-		const mapFilesData: string[] = [];
-		for (let i = 1; i <= mapCount; i++) {
-			mapFilesData.push(`map${i}.json`);
-		}
-		setMapFiles(mapFilesData);
-	}, []);
+    const perPage = 20;
+    const startIndex = currentPage * perPage;
+    const endIndex = Math.min((currentPage + 1) * perPage, mapFiles.length);
 
-	const handlePrevClick = () => {
-		setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
-		playSound("swoosh", 0.15);
-	};
+    useEffect(() => {
+        // Set mapFiles with some map data
+        const mapFilesData: string[] = [];
+        for (let i = 1; i <= mapCount; i++) {
+            mapFilesData.push(`map${i}.json`);
+        }
+        setMapFiles(mapFilesData);
+    }, [mapCount]);
 
-	const handleNextClick = () => {
-		setCurrentPage((prevPage) =>
-			Math.min(prevPage + 1, Math.ceil(mapFiles.length / perPage) - 1)
-		);
-		playSound("swoosh", 0.15);
-	};
+    const handlePrevClick = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+        playSound("swoosh", 0.15);
+    };
 
-	const handleStartClick = () => {
-		onPageChange("start");
-		playSound("click", 0.25);
-	};
+    const handleNextClick = () => {
+        setCurrentPage((prevPage) =>
+            Math.min(prevPage + 1, Math.ceil(mapFiles.length / perPage) - 1)
+        );
+        playSound("swoosh", 0.15);
+    };
 
-	const handlePlayClick = () => {
-		onPageChange("play");
-		playSound("click", 0.25);
-		playSound("levelstart", 0.5);
-	};
+    const handleStartClick = () => {
+        onPageChange("start");
+        playSound("click", 0.25);
+    };
 
-	const handleCreditsClick = () => {
-		onPageChange("credits");
-		playSound("click", 0.25);
-		playSound("swoosh", 0.15);
-	};
+    const handlePlayClick = () => {
+        onPageChange("play");
+        playSound("click", 0.25);
+        playSound("levelstart", 0.5);
+    };
 
-	function handleMouseOver() {
-		playSound("hover", 0.15);
-	}
+    const handleCreditsClick = () => {
+        onPageChange("credits");
+        playSound("click", 0.25);
+        playSound("swoosh", 0.15);
+    };
 
-	return (
-		<>
-			<div id="selectlevel">
-				<h1>Select Level</h1>
+    function handleMouseOver() {
+        playSound("hover", 0.15);
+    }
 
-				<div className="levels">
-					<ul>
-						{mapFiles.slice(startIndex, endIndex).map((map, index) => (
-							<li
-								key={startIndex + index}
-								data-name={map}
-								onMouseOver={map === "map1.json" ? handleMouseOver : undefined}
-								className={map !== "map1.json" ? "notplayable" : ""}
-							>
-								{startIndex + index + 1}
-								<span>2 moves</span>
-							</li>
-						))}
-					</ul>
-					<button
-						className="arrow prev"
-						onClick={handlePrevClick}
-						onMouseOver={handleMouseOver}
-						disabled={currentPage === 0}
-					></button>
+    return (
+        <>
+            <div id="selectlevel">
+                <h1>Select Level</h1>
 
-					<button
-						className="arrow next"
-						onClick={handleNextClick}
-						onMouseOver={handleMouseOver}
-						disabled={currentPage === Math.ceil(mapFiles.length / perPage) - 1}
-					></button>
-				</div>
-				<div id="menubuttons">
-					<span>{`Page ${currentPage + 1} of ${Math.ceil(
-						mapFiles.length / perPage
-					)}`}</span>
-					<div
-						id="btn-start"
-						className="button"
-						onClick={handleStartClick}
-						onMouseOver={handleMouseOver}
-					></div>
-					<div
-						id="btn-credits"
-						className="button"
-						onClick={handleCreditsClick}
-						onMouseOver={handleMouseOver}
-					></div>
-					<div
-						id="btn-play"
-						className="button"
-						onClick={handlePlayClick}
-						onMouseOver={handleMouseOver}
-					></div>
-				</div>
-			</div>
-		</>
-	);
+                <div className="levels">
+                    <ul>
+                        {mapFiles.slice(startIndex, endIndex).map((map, index) => (
+                            <li
+                                key={startIndex + index}
+                                data-name={map}
+                                onMouseOver={map === "map1.json" ? handleMouseOver : undefined}
+                                className={map !== "map1.json" ? "notplayable" : ""}
+                            >
+                                {startIndex + index + 1}
+                                <span>2 moves</span>
+                            </li>
+                        ))}
+                    </ul>
+                    <button
+                        className="arrow prev"
+                        onClick={handlePrevClick}
+                        onMouseOver={handleMouseOver}
+                        disabled={currentPage === 0}
+                    ></button>
+
+                    <button
+                        className="arrow next"
+                        onClick={handleNextClick}
+                        onMouseOver={handleMouseOver}
+                        disabled={currentPage === Math.ceil(mapFiles.length / perPage) - 1}
+                    ></button>
+                </div>
+                <div id="menubuttons">
+                    <span>{`Page ${currentPage + 1} of ${Math.ceil(
+                        mapFiles.length / perPage
+                    )}`}</span>
+                    <div
+                        id="btn-start"
+                        className="button"
+                        onClick={handleStartClick}
+                        onMouseOver={handleMouseOver}
+                    ></div>
+                    <div
+                        id="btn-credits"
+                        className="button"
+                        onClick={handleCreditsClick}
+                        onMouseOver={handleMouseOver}
+                    ></div>
+                    <div
+                        id="btn-play"
+                        className="button"
+                        onClick={handlePlayClick}
+                        onMouseOver={handleMouseOver}
+                    ></div>
+                </div>
+            </div>
+        </>
+    );
 }
