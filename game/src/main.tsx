4a2b8196c9b62f68f4b8d5861e8895d937b515cc
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-19 11:38:14 +0100

    Context

--- game/src/main.tsx
+++ game/src/main.tsx
@@ -1,10 +1,14 @@
+import './index.css'
+
+import App from './App.tsx'
+import { GameContextProvider } from './ContextProvider/ContextProvider.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GameContextProvider>
+      <App />
+    </GameContextProvider>
   </React.StrictMode>,
 )
