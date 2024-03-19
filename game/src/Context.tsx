1c1d07c9341aae939326f5f854a51835023748f6
mansooriabbas <mansoori.abbas@outlook.com>
2024-03-19 11:47:38 +0100

    latest-deleted-leveldisplay

---
+++ game/src/Context.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react';
+
+const gameContext = createContext();
+
+const globalState = {
+    text: "from context"
+}
+
+const gameContextProvider = ({ children }) => {
+    const [state, setState] = useState(globalState);
+  
+    return (
+      <gameContext.Provider value={{ state, setState }}>
+        {children}
+      </gameContext.Provider>
+    );
+  };
+  
+  export { gameContext, gameContextProvider };
