c5be09fb9b6157bde6d030c10fb460c257e74695
Fredrik Berglund <fredrik@diam.se>
2024-03-19 14:10:20 +0100

    Small fixes

--- game/src/ContextProvider/ContextProvider.tsx
+++ game/src/ContextProvider/ContextProvider.tsx
@@ -1,25 +1,28 @@
 import { createContext, useState } from "react";
 
 interface GameContextProps {
-  test: string;
-  setTest: (test: string) => void;
+	test: string;
+	setTest: (test: string) => void;
+	otherTest: string;
+	setOtherTest: (otherTest: string) => void;
 }
 
-
 interface ChildrenProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export const MyContext = createContext({} as GameContextProps);
 
-
 export const GameContextProvider = ({ children }: ChildrenProps) => {
-  const [state, setState] = useState("Test");
-  console.log(state);
+	const [test, setTest] = useState("Test");
+	const [otherTest, setOtherTest] = useState("OtherTest");
 
-  return (
-    <MyContext.Provider value={{ test: state, setTest: setState }}>
-      {children}
-    </MyContext.Provider>
-  );
+	const value: GameContextProps = {
+		test,
+		setTest,
+		otherTest,
+		setOtherTest,
+	};
+
+	return <MyContext.Provider value={value}>{children}</MyContext.Provider>;
 };
