5a2698c766c9f85a0567ccddd601f1451b9b467e
mansooriabbas <mansoori.abbas@outlook.com>
2024-03-21 13:22:28 +0100

    reverted changes

--- game/src/components/Music.tsx
+++ game/src/components/Music.tsx
@@ -6,63 +6,52 @@ import playMusic2 from "./../assets/risk-136788.mp3";
 import playMusic3 from "./../assets/thinking-time-148496.mp3";
 
 interface MusicProps {
-    audio: string;
+	audio: string;
 }
 
 export const Music: React.FC<MusicProps> = ({ audio }) => {
-    const [music, setMusic] = useState<string>(""); // Initialize music state with null
-    const { level, isMuted } = useContext(MyContext);
-    const audioRef = useRef<HTMLAudioElement>(null);
+	const [music, setMusic] = useState<string>(""); // Initialize music state with null
+	const { level, isMuted } = useContext(MyContext);
+	const audioRef = useRef<HTMLAudioElement>(null);
 
-    useEffect(() => {
-        // Set the music based on the audio prop
-        if (audio === "ui") {
-            setMusic(uiMusic);
-        } else if (audio === "play") {
-            if (level <= 8) {
-                setMusic(playMusic);
-            } else if (level > 8 && level <= 19) {
-                setMusic(playMusic2);
-            } else if (level > 19) {
-                setMusic(playMusic3);
-            }
-        }
-    }, [audio, level]);
+	useEffect(() => {
+		// Set the music based on the audio prop
+		if (audio === "ui") {
+			setMusic(uiMusic);
+		} else if (audio === "play") {
+			if (level <= 8) {
+				setMusic(playMusic);
+			} else if (level > 8 && level <= 19) {
+				setMusic(playMusic2);
+			} else if (level > 19) {
+				setMusic(playMusic3);
+			}
+		}
+	}, [audio]);
 
-    useEffect(() => {
-        // Play music when music state changes
-        const musicPlayer = audioRef.current;
+	useEffect(() => {
+		// Play music when music state changes
+		const musicPlayer = audioRef.current;
 
-        if (music && musicPlayer && !isMuted) {
-            musicPlayer.src = music;
-            musicPlayer.loop = true;
-            musicPlayer.play().catch(error => {
-                console.error("Failed to play music:", error);
-            });
-        }
+		if (music && musicPlayer) {
+			musicPlayer.src = music;
+			if (isMuted) {
+				musicPlayer.volume = 0;
+			} else {
+				musicPlayer.volume = 0.15;
+			}
 
-        return () => {
-            // Cleanup function to pause music when component unmounts
-            if (musicPlayer && !musicPlayer.paused) {
-                musicPlayer.pause();
-            }
-        };
-    }, [music, isMuted]);
+			musicPlayer.loop = true;
+			musicPlayer.play();
+		}
 
-    return (
-        <button onClick={playAudio} style={{ display: "none" }}>
-            Play Audio
-        </button>
-    );
+		return () => {
+			// Cleanup function to pause music when component unmounts
+			if (musicPlayer && !musicPlayer.paused) {
+				musicPlayer.pause();
+			}
+		};
+	}, [music]);
 
-    function playAudio() {
-        const musicPlayer = audioRef.current;
-        if (musicPlayer) {
-            musicPlayer.play().catch(error => {
-                console.error("Failed to play music:", error);
-            });
-        }
-    }
-
-    return <audio id="music" autoPlay loop ref={audioRef} />;
+	return <audio id="music" autoPlay loop ref={audioRef} />;
 };
