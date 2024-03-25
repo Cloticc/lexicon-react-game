b51f749905a89026c116432118ee7d72eac49763
Alireza KafshdarToosi <a.k.toosi@gmail.com>
2024-03-25 13:05:57 +0100

    Added temp button to save current history array in a json file

--- game/src/pages/Play.tsx
+++ game/src/pages/Play.tsx
@@ -1,55 +1,76 @@
-import "./../css/Play.css";
+import './../css/Play.css'
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect } from 'react'
 
-import { Highscore } from "./Highscore";
-import { MapRender } from "../components/MapRender";
-import { MyContext } from "../ContextProvider/ContextProvider";
-import { SelectPageProps } from "./../components/InterfacePages";
-import allMaps from "./../maps/maps";
-import { formatElapsedTime } from "../utils/TimeUtils";
-import { playSound } from "./../components/playSound";
+import { Highscore } from './Highscore'
+import { MapRender } from '../components/MapRender'
+import { MyContext } from '../ContextProvider/ContextProvider'
+import { SelectPageProps } from './../components/InterfacePages'
+import allMaps from './../maps/maps'
+import { formatElapsedTime } from '../utils/TimeUtils'
+import { playSound } from './../components/playSound'
 
 export function Play({ onPageChange }: SelectPageProps) {
-	const {
-		counter,
-		elapsedTime,
-		wonGame,
-		level,
-		setMusic,
-		setShowGameContainer,
-		resetGame,
-		setHandleHistory,
-		gameRunning,
-	} = useContext(MyContext);
+    const {
+        counter,
+        elapsedTime,
+        wonGame,
+        level,
+        setMusic,
+        setShowGameContainer,
+        resetGame,
+        setHandleHistory,
+        gameRunning,
+        history,
+    } = useContext(MyContext)
 
-	useEffect(() => {
-		setMusic("play");
-		setShowGameContainer(true);
-	}, []);
+    useEffect(() => {
+        setMusic('play')
+        setShowGameContainer(true)
+    }, [])
 
-	const handleSelectLevelClick = () => {
-		resetGame();
-		onPageChange("selectlevel");
-		playSound("click", 0.25);
-		playSound("swoosh", 0.15);
-	};
-	const handleSpacePress = () => {
-		// setHandleHistory(true);
+    const handleSelectLevelClick = () => {
+        resetGame()
+        onPageChange('selectlevel')
+        playSound('click', 0.25)
+        playSound('swoosh', 0.15)
+    }
+    const handleSpacePress = () => {
+        // setHandleHistory(true);
 
-  const event = new KeyboardEvent('keydown', {
-    key: ' ', 
-  });
+        const event = new KeyboardEvent('keydown', {
+            key: ' ',
+        })
 
-  // Dispatch the event
-  window.dispatchEvent(event);
-	};
+        // Dispatch the event
+        window.dispatchEvent(event)
+    }
 
-	function handleMouseOver() {
-		playSound("hover", 0.15);
-	}
+    function handleMouseOver() {
+        playSound('hover', 0.15)
+    }
 
-	/*
+    const saveSolutionToJson = () => {
+        const jsonSolution = JSON.stringify(history)
+        const blob = new Blob([jsonSolution], { type: 'application/json' })
+        console.log(history)
+
+        // Create a temporary anchor element to trigger the download
+        const a = document.createElement('a')
+        a.href = URL.createObjectURL(blob)
+
+        // Set the filename based on the current level
+        a.download = `level_${level+1}_solution.json`
+
+        // Trigger the download
+        a.click()
+
+        // Release the object URL
+        a.remove()
+        window.URL.revokeObjectURL(a.href)
+    }
+
+    /*
 	// Can remove this useEffect. It's just to show the highscore element after 3 seconds
 	useEffect(() => {
 		setTimeout(() => {
@@ -58,32 +79,41 @@ export function Play({ onPageChange }: SelectPageProps) {
 	});
 */
 
-	return (
-		<>
-			<div id="showlevel">Level {level + 1}</div>
-			<div id="status">
-				<div id="stepstaken">{counter} steps</div>
-				<div id="timer">{formatElapsedTime(elapsedTime)}</div>
-			</div>
+    return (
+        <>
+            <div id="showlevel">Level {level + 1}</div>
+            <div id="status">
+                <div id="stepstaken">{counter} steps</div>
+                <div id="timer">{formatElapsedTime(elapsedTime)}</div>
+            </div>
 
-			{gameRunning && (
-				<button
-					id="btn-undostep"
-					className="button"
-					onMouseOver={handleMouseOver}
-					onClick={handleSpacePress}
-				></button>
-			)}
+            {gameRunning && (
+                <button
+                    id="btn-undostep"
+                    className="button"
+                    onMouseOver={handleMouseOver}
+                    onClick={handleSpacePress}
+                ></button>
+            )}
 
-			<button
-				id="btn-selectlevel"
-				className="button"
-				onMouseOver={handleMouseOver}
-				onClick={handleSelectLevelClick}
-			></button>
+            <button
+                id="btn-selectlevel"
+                className="button"
+                onMouseOver={handleMouseOver}
+                onClick={handleSelectLevelClick}
+            ></button>
 
-			<MapRender initialMapData={allMaps[level].mapdata} />
-			{wonGame && <Highscore />}
-		</>
-	);
+            <button
+                id="btn-btn-solution"
+                className="button"
+                onMouseOver={handleMouseOver}
+                onClick={saveSolutionToJson}
+            >
+                Save Solution
+            </button>
+
+            <MapRender initialMapData={allMaps[level].mapdata} />
+            {wonGame && <Highscore />}
+        </>
+    )
 }
