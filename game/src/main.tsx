4eb61c34cc9e828ebd22c4c2a59e3fa066dfeac6
Fredrik Berglund <fredrik@diam.se>
2024-04-04 19:09:16 +0200

    Update

--- game/src/main.tsx
+++ game/src/main.tsx
@@ -1,15 +1,17 @@
-import './index.css'
+import './index.css';
 
-import App from './App.tsx'
-import { GameContextProvider } from './ContextProvider/ContextProvider.tsx'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import App from './App.tsx';
+import { GameContextProvider } from './ContextProvider/ContextProvider.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GameContextProvider>
-      <App />
-    </GameContextProvider>
-  </React.StrictMode>,
-)
-
+    <HashRouter>
+        <React.StrictMode>
+            <GameContextProvider>
+                <App />
+            </GameContextProvider>
+        </React.StrictMode>
+    </HashRouter>
+);
