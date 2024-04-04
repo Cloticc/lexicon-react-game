545ebbd9913a7cc1293b4ced4c61269920df7de6
Andreas Isidorsson <isidorssona@gmail.com>
2024-04-04 17:25:47 +0200

    hack fix maps

--- game/src/maps/maps.ts
+++ game/src/maps/maps.ts
@@ -4,21 +4,23 @@ export interface MapType {
     solution: any[];
 }
 
-const mapFiles = await import.meta.glob('../maps/*.json');
+const mapFiles = import.meta.glob('../maps/*.json');
 
 const allMaps: MapType[] = [];
 
-const paths = Object.keys(mapFiles).sort((a, b) => {
-    const numA = parseInt(a.match(/\d+/)![0]);
-    const numB = parseInt(b.match(/\d+/)![0]);
-    return numA - numB;
-});
+(async () => {
+    const paths = Object.keys(mapFiles).sort((a, b) => {
+        const numA = parseInt(a.match(/\d+/)![0]);
+        const numB = parseInt(b.match(/\d+/)![0]);
+        return numA - numB;
+    });
 
-for (const path of paths) {
-    // Explicitly specify the type of mapData using type assertion or casting
-    const mapData = (await mapFiles[path]()) as MapType; // Type assertion
-    // const mapData: MapType = await mapFiles[path]() as MapType; // Casting
-    allMaps.push(mapData);
-}
+    for (const path of paths) {
+        // Explicitly specify the type of mapData using type assertion or casting
+        const mapData = (await mapFiles[path]()) as MapType; // Type assertion
+        // const mapData: MapType = await mapFiles[path]() as MapType; // Casting
+        allMaps.push(mapData);
+    }
+})();
 
 export default allMaps;
